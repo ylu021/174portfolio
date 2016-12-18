3416d28b9595d96712036db0a761cb7e93706b37
ylu021 <yichen_0@hotmail.com>
2016-12-17 23:34:34 -0500

    add plugin

--- portfolio.php
+++ portfolio.php
@@ -2,52 +2,27 @@
 
     <!-- Portfolio Section -->
     <section id="portfolio">
-        <div class="container">
             <h1>Portfolio</h1> 
             <div class="content">
-                <article>
-                    <div class="article-container">
-                        <figure class="box">
-                            <img src="http://urcsc170.org/mtam2/project1-team7/images/pokemon_logo.png" alt="Fig1. - Pokémon Generations">
-                            <figcaption>Pokémon Generations</figcaption>
-                        </figure>
-                        <div>   
-                            <h2>Pokémon Generations</h2>
-                            <p>This is a reference website for all Pokémon lovers; Advanced Front-end Web Design and Development team project 1.</p>
-                            <a href="http://urcsc170.org/mtam2/project1-team7/">Learn More</a>
-                        </div>
-                    </div>
-                    
-                    <div class="article-container">
-                        <figure class="box">
-                            <img src="http://urcsc170.org/mtam2/project2-team7/images/logo.png" alt="Fig2. - Hero Central.">
-                            <figcaption>Hero Central</figcaption>
-                        </figure>
-                        <div>
-                            <h2>Hero Central</h2>
-                            <p>This is a reference website for all Super Hero lovers; Advanced Front-end Web Design and Development team project 2 (A modification on team 1's project 1). </p>
-                            <a href="http://urcsc170.org/mtam2/project2-team7/">Learn More</a>
-                        </div>
-                    </div>
-                    
-                    <div class="article-container">
-                        <figure class="box">
-                            <img src="http://www.ichiran.co.jp/img/ichiran.gif" alt="Fig3. - Freshly Made Noodles.">
-                            <figcaption>Ichiran Remodification</figcaption>
-                        </figure>
-                        <div>
-                            <h2>Ichiran Remodification</h2>
-                            <p>This is a remodification of Ichiran's website. While the details are ignored, we propose the use of Z and F patterns to make the brand more modern.</p>
-                            <a href="http://urcsc170.org/xli/Ichiran/">Learn More</a>
-                        </div>
-                    </div>
-
-                </article>
-
+                <ul id="og-grid" class="og-grid">
+                    <li>
+                        <a href="http://urcsc170.org/mtam2/project1-team7" data-largesrc="http://urcsc170.org/mtam2/project1-team7/images/pokemon_logo.png" data-title="Pokémon Generations" data-description="This is a reference website for all Pokémon lovers; Advanced Front-end Web Design and Development team project 1.">
+                            <img src="http://urcsc170.org/mtam2/project1-team7/images/pokemon_logo.png" alt="Pokémon Generations"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://urcsc170.org/mtam2/project2-team7" data-largesrc="http://urcsc170.org/mtam2/project2-team7/images/logo.png" data-title="Hero Central" data-description="This is a reference website for all Super Hero lovers; Advanced Front-end Web Design and Development team project 2 (A modification on team 1's project 1).">
+                            <img src="http://urcsc170.org/mtam2/project2-team7/images/logo.png" alt="Hero Central"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://urcsc170.org/xli/Ichiran/" data-largesrc="http://www.ichiran.co.jp/img/ichiran.gif" data-title="Ichiran Remodification" data-description="This is a remodification of Ichiran's website. While the details are ignored, we propose the use of Z and F patterns to make the brand more modern.">
+                            <img src="http://www.ichiran.co.jp/img/ichiran.gif" alt="Ichiran Remodification"/>
+                        </a>
+                    </li>
+                    <!-- ... -->
+                </ul>
             </div><!-- .content -->
-
-       </div><!-- .container -->
-
     </section>
     
 <?php include "inc/footer.inc"; ?>
