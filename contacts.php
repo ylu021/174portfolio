6270cc486673918a7891e98fa7e8116711e4d909
ylu021 <yichen_0@hotmail.com>
2016-12-17 21:41:31 -0500

    editing css

--- contacts.php
+++ contacts.php
@@ -2,41 +2,41 @@
 	
 	<section id="contacts">
         <div class="container">
-			<h1>Contact</h1>
+			<h1>Contacts</h1>
 			<form name="myForm" method="post" action="database-write.php" onsubmit="return(validate());">
 				<!-- name -->
 				<div class="row">
 					<div>
 						<!-- firstname -->
 						<div class="form-group">
-							<label class="sr-only" for="firstname">First Name </label>
-							<input class="form-control" type="text" name="firstname" required>
+							<label for="firstname">First Name </label>
+							<input class="form-text" type="text" name="firstname" required>
 						</div>
 					</div>
 					<div>
 						<!-- lastname -->
 						<div class="form-group">
-							<label class="sr-only" for="lastname">Last Name </label>
-							<input class="form-control" type="text" name="lastname" required>
+							<label for="lastname">Last Name </label>
+							<input class="form-text" type="text" name="lastname" required>
 						</div>
 					</div>
 				</div>
 				
 				<!-- email -->
 				<div class="form-group">
-					<label class="sr-only" for="email">Email </label>
-					<input class="form-control" type="text" name="email" required>
+					<label for="email">Email </label>
+					<input class="form-text" type="text" name="email" required>
 				</div>
 				<!-- phone -->
 				<div class="form-group">
 					<label class="sr-only" for="phone">Phone </label>
-					<input class="form-control" type="text" name="phone" required>
+					<input class="form-text" type="text" name="phone" required>
 				</div>
 				<!-- message -->
 				<div class="form-group">
-					<textarea placeholder="Describe yourself here..." class="form-control" name="message" id="message" rows="5"></textarea>
+					<textarea placeholder="Describe yourself here..." name="message" id="message" rows="5"></textarea>
 				</div>
-				<input class="ghost-button" type="submit" value="Send" />
+				<input id="submit" class="ghost-button" type="submit" value="Send" />
 			</form>
 		</div>
 	</section>
